f41bfdc31d442b6d3e67d7676a3b2248d2aba635
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2023-05-25 20:29:55 +0530

    Add files via upload

---
+++ sixth_day.py
@@ -0,0 +1,12 @@
+print("First we have to boil the water!!!")
+a=int(input("Enter 1 for water is boiled or 0 for water is still unboiled:"))
+if a==1:
+    print("Add tea leaves into the boiled water")
+else:
+    print("Let the water boil properly first")  
+print("After boiling add milk into it!!!")
+b=int(input("Enter 1 for milk is added or 0 for milk is not added:"))
+if b==1:
+    print("Your tea is now prepared now you can drink it")
+else:
+    print("Add milk into your tea")        
